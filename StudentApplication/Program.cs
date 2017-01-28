d7d2043e83d87faddabd63399944f89208a3d359
noora Jokinen <K8241@student.jamk.fi>
2017-01-28 13:39:08 +0200

    Start of StudentApplication

---
+++ StudentApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Name = "Mickey Mouse";
+            student.Address = "Disneyland, California";
+            student.PhoneNumber = 764870;
+            student.StudentNumber = 400;
+            student.Class = "J60";
+            student.Study();
+        }
+    }
+}
---
+++ StudentApplication/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApplication
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public int StudentNumber { get; set; }
+        public string Class { get; set; }
+
+        public void Study()
+        {
+            Console.WriteLine("I'm studying");
+        }
+
+        public void SkipStudy()
+        {
+            Console.WriteLine("I feel lazy today");
+        }
+
+
+    }
+}
